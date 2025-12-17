0339895d74369540cd8fa8d32065f30411c308ce
Thuan Anh Bui <tb24600@essex.ac.uk>
2025-12-17 15:50:20 +0000

    Fixed path for CARLA dataset and remove previously-run result in TriAD

--- CARLA_Modified/utils/mypath.py
+++ CARLA_Modified/utils/mypath.py
@@ -1,32 +1,15 @@
 
 import os
-
+from pathlib import Path
 
 class MyPath(object):
     @staticmethod
     def db_root_dir(database=''):
-        db_names = {'msl', 'smap', 'smd', 'power', 'yahoo', 'kpi', 'swat', 'wadi', 'gecco', 'swan', 'ucr', 'smart_grid'}
+        db_names = {'smart_grid'}
         assert(database in db_names)
 
-        if database == 'msl' or database == 'smap':
-            return r"D:\Program Files (x86)\OneDrive\Documents\University of Essex\Dissertation\CARLA\datasets\MSL_SMAP"
-        elif database == 'ucr':
-            return '/home/zahraz/hz18_scratch/zahraz/datasets/UCR'
-        elif database == 'yahoo':
-            return '/home/zahraz/hz18_scratch/zahraz/datasets/Yahoo'
-        elif database == 'smd':
-            return '/home/zahraz/hz18_scratch/zahraz/datasets/SMD'
-        elif database == 'swat':
-            return '/home/zahraz/hz18_scratch/zahraz/datasets/SWAT'
-        elif database == 'wadi':
-            return '/home/zahraz/hz18_scratch/zahraz/datasets/WADI'
-        elif database == 'kpi':
-            return r"D:\Program Files (x86)\OneDrive\Documents\University of Essex\Dissertation\CARLA\datasets\kpi"
-        elif database == 'swan':
-            return '/home/zahraz/hz18_scratch/zahraz/datasets/Swan'
-        elif database == 'gecco':
-            return '/home/zahraz/hz18_scratch/zahraz/datasets/GECCO'
-        elif database == 'smart_grid':
-            return r"D:\Program Files (x86)\OneDrive\Documents\University of Essex\Dissertation\CARLA\datasets\SmartGrid"
+        if database == 'smart_grid':
+            BASE_DIR = Path(__file__).resolve().parent.parent
+            return BASE_DIR / "datasets" / "SmartGrid"
         else:
             raise NotImplementedError
